052822d258455328d868a018a3dd188f1ab49d4d
Allikk38 <alexanderfobia@yandex.ru>
2026-04-20 13:33:47 +0700

    Create PermissionManager.js

---
+++ core/PermissionManager.js
@@ -0,0 +1,41 @@
+import { SupabaseClient } from './SupabaseClient.js';
+import { EventBus } from './EventBus.js';
+
+class PermissionManagerClass {
+    constructor() {
+        this.permissions = new Set();
+        this.loaded = false;
+    }
+
+    async loadUserPermissions(userId) {
+        const { data: profile } = await SupabaseClient
+            .from('profiles')
+            .select('role_id')
+            .eq('id', userId)
+            .single();
+
+        if (!profile) return;
+
+        const { data: perms } = await SupabaseClient
+            .from('role_permissions')
+            .select('permissions(slug)')
+            .eq('role_id', profile.role_id);
+
+        this.permissions.clear();
+        perms?.forEach(p => this.permissions.add(p.permissions.slug));
+        this.loaded = true;
+        
+        EventBus.emit('permissions:loaded');
+    }
+
+    can(slug) {
+        return this.loaded && this.permissions.has(slug);
+    }
+
+    clear() {
+        this.permissions.clear();
+        this.loaded = false;
+    }
+}
+
+export const PermissionManager = new PermissionManagerClass();
